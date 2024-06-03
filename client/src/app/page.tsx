6cd30927f767d6aac8aae49aa2ecf83ef5a7c2d4
Vlone Me <francisntima@gmail.com>
2024-06-03 06:18:13 -0400

    Client Initial Commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='text-white-1'>Home 
+    <Button className='bg-orange-1'>Click Me</Button></div>
+  )
+}
